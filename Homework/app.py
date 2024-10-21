4d10c41a16c6384b6f750e0f82260a500f9fe217
Bogdan Mihaica <claudiumihaica12@gmail.com>
2024-10-21 21:24:52 +0300

    a

--- Homework/app.py
+++ Homework/app.py
@@ -9,20 +9,17 @@ file = pd.read_excel(filename)
 
 
 #check errors
+def check_errors(file):
+    null_vals=[col for col,val in file.isnull().sum().items()if int(val) != 0]
+    dupes= [col for col,val in file.duplicated().items() if val=="True"]
 
+    if len(null_vals)!=0:
+        for val in null_vals:
+            print(f"Column '{val}' has null values")
+    else:
+        print("There are no columns with null values in the current dataset")
 
-null_vals=[col for col,val in file.isnull().sum().items()if int(val) != 0]
-dupes= [col for col,val in file.duplicated().items() if val=="True"]
-
-
-
-if len(null_vals)!=0:
-    for val in null_vals:
-        print(f"Column '{val}' has null values")
-else:
-    print("There are no columns with null values in the current dataset")
-
-if len(dupes)!=0:
-    print(f"The following entries: {dupes} have duplicates")
-else:
-    print("There are no duplicates in the current dataset")
+    if len(dupes)!=0:
+        print(f"The following entries: {dupes} have duplicates")
+    else:
+        print("There are no duplicates in the current dataset")
