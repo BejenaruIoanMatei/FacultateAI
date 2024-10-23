562888c990bb206ef2ce2b878708d5f25bf3c658
RaresPipirig <126176646+RaresPipirig@users.noreply.github.com>
2024-10-23 22:51:51 +0300

    Revert "Merge branch 'main' of https://github.com/BejenaruIoanMatei/FacultateAI"
    This reverts commit f2905e6e89512a726be3e4b69f592aa8b3e052e4, reversing
    changes made to 8f890bc9b247c0cd6328999d8ad03ed11bf54734.

--- Homework/app.py
+++ Homework/app.py
@@ -1,129 +1,32 @@
 import pandas as pd
 import os
-import pprint as pp
 
-races={
-    "BEN": "Bengal",
-    "SBI": "Birman",
-    "BRI": "British Shorthair",
-    "CHA": "Chartreux",
-    "EUR": "European",
-    "MCO": "Maine coon",
-    "PER": "Persian",
-    "RAG": "Ragdoll",
-    "SPH": "Sphynx",
-    "ORI": "Siamese",
-    "TUV": "Turkish angora",
-    "Autre": "Other",
-    "NSP": "Unknown",
-    "NR":"No race",
-    "SAV":"Savannah"
-}
-def get_data():
+# Funcție care citește datele și verifică erorile
+def load_and_check_data():
     here = os.path.dirname(os.path.abspath(__file__))
     filename = os.path.join(here, 'dataset.xlsx')
+
+    # Citirea fișierului Excel
     file = pd.read_excel(filename)
-    return file
-
-def get_file(name, mode='r', content_to_write=None):
-    here = os.path.dirname(os.path.abspath(__file__))
-    filename = os.path.join(here, name)
-    with open(filename, mode) as file:
-        if 'r' in mode:
-            return file.read()
-        elif 'w' in mode or 'a' in mode:
-            file.write(str(content_to_write))
-            return f"Content written to {filename}"
-
-def number_of_instances():
-    file=get_data()
-    races_count = dict()
     
-    for index, line in file.iterrows():
-        race=line['Race']
-        if races[race] in races_count:
-            races_count[races[race]]+=1
-        else: 
-            races_count[races[race]]=1
-    return races_count
-
-def fetch_for_column(column):
-    file=get_data()
-    col_dict=dict()
-    for _, line in file.iterrows():
-        col_val=line[column]
-        if col_val in col_dict:
-            col_dict[col_val]+=1
-        else: 
-            col_dict[col_val]=1
-    return col_dict
-
-def get_attributes():
-    file=get_data()
-    data_cols=file.columns
-    col_arr=[]
-    for index, column in enumerate(data_cols):
-        if index>1 and index<len(data_cols)-1:
-            col_arr.append(column)
-    return col_arr
-
-def number_of_unique_instances_races():
-    unique_data = dict()
-    file = get_data()
-    col_arr = get_attributes()  
-
-    for _, line in file.iterrows():
-        race = line['Race']
-        if race not in unique_data:
-            unique_data[race] = {col: dict() for col in col_arr if col != 'Race'}
-
-        for col in col_arr:
-            if col != 'Race':
-                attribute_value = line[col]
-                if attribute_value not in unique_data[race][col]:
-                    unique_data[race][col][attribute_value] = 0
-                unique_data[race][col][attribute_value] += 1
-
-    return unique_data
-
-
-
-
-def number_of_unique_instances_file():
-    unique_attr=dict()
-    file=get_data()
-    col_arr=get_attributes()
-
-
-    for _, line in file.iterrows():
-        for col in col_arr:
-            if col not in unique_attr.keys():
-                unique_attr[col]=dict()
-            if line[col] not in unique_attr[col].keys():
-                unique_attr[col][line[col]]=0
-            unique_attr[col][line[col]]+=1
-   
-    return unique_attr
-
-
-def load_and_check_data():
-    file=get_data()
-    
+    # Afișarea numelor coloanelor din dataset pentru verificare
     print("Coloanele din dataset:", file.columns)
 
+    # Verificarea valorilor lipsă și duplicate
     null_vals = [col for col, val in file.isnull().sum().items() if int(val) != 0]
     dupes = file[file.duplicated()]
-    
+
+    # Afișarea erorilor
     if len(null_vals) != 0:
         for val in null_vals:
-            print(f"Coloana '{val}' are valori lipsa.")
+            print(f"Coloana '{val}' are valori lipsă.")
     else:
-        print("Nu exista coloane cu valori lipsa in dataset.")
+        print("Nu există coloane cu valori lipsă în dataset.")
 
     if len(dupes) != 0:
-        print(f"Exista {len(dupes)} intrari duplicate in dataset.")
+        print(f"Există {len(dupes)} intrări duplicate în dataset.")
     else:
-        print("Nu exista duplicate in dataset.")
+        print("Nu există duplicate în dataset.")
 
-
-pp.pprint(number_of_unique_instances_file())
+    # Returnează dataset-ul pentru a fi folosit în alte funcții
+    return file
